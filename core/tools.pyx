0b95f522f46a38636e673499fd1104ea35988b4c
Tammy Yang <tammy@dt42.io>
2016-02-16 16:15:35 +0800

    add rebin2D to DATA

--- core/tools.pyx
+++ core/tools.pyx
@@ -344,6 +344,42 @@ class DATA(TOOLS):
         """Conditions to select entries for sign_diff"""
         return x.weekday()
 
+    def rebin2D(self, ndarray, new_shape, operation='sum'):
+        """
+        Bins an ndarray in all axes based on the target shape,
+            by summing or averaging.
+
+        Number of output dimensions must match number of input dimensions.
+
+        Example
+        -------
+        >>> m = np.arange(0,100,1).reshape((10,10))
+        >>> n = bin_ndarray(m, new_shape=(5,5), operation='sum')
+        >>> print(n)
+
+        [[ 22  30  38  46  54]
+         [102 110 118 126 134]
+         [182 190 198 206 214]
+         [262 270 278 286 294]
+         [342 350 358 366 374]]
+
+        """
+        if not operation.lower() in ['sum', 'mean', 'average', 'avg']:
+            raise ValueError("Operation not supported.")
+        if ndarray.ndim != len(new_shape):
+            raise ValueError("Shape mismatch: {} -> {}".format(ndarray.shape,
+                                                               new_shape))
+        compression_pairs = [(d, c//d) for d,c in zip(new_shape,
+                                                      ndarray.shape)]
+        flattened = [l for p in compression_pairs for l in p]
+        ndarray = ndarray.reshape(flattened)
+        for i in range(len(new_shape)):
+            if operation.lower() == "sum":
+                ndarray = ndarray.sum(-1*(i+1))
+            elif operation.lower() in ["mean", "average", "avg"]:
+                ndarray = ndarray.mean(-1*(i+1))
+        return ndarray
+
     def convert_cats(self, y):
         """Convert target from [1,2,0...] to
            [[0,1,0], [0,0,1], [1,0,0]...]"""
