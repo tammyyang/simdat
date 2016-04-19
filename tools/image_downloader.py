10136a40b6e59216c03146d257c97c14ab5f03b4
Tammy Yang <tammy@dt42.io>
2016-04-19 12:39:51 +0800

    use icrawler to download images from Bing

--- tools/image_downloader.py
+++ tools/image_downloader.py
@@ -1,30 +1,34 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
+#!/usr/bin/env python3
 
 '''
-This is a modified version of
-https://github.com/hardikvasa/google-images-download
+This is a simple script to download images from Bing.
+One can also replace BingImageCrawler by other crawler easily,
+just go to https://pypi.python.org/pypi/icrawler/0.1.5
+and you should find easily how to achieve it.
 
-Before starting, prepare the download_args.json with the
-keywords you want to search for. For example:
-{
-    "search_for": {
-        "Australia": ["high resolution", "paintings"],
-        "Pyramid of Giza": ["at night", "from top"]
-    }
-}
+Usage:
+    0. Install icrawler
+        $pip3 install icrawler
+    1. Create download_args.json to contain keywords you want to use:
+        {
+         "search_for": {
+                        "apple": ["fruit", "pie"],
+                        "banana": []
+                       }
+        }
+    2. Execute the script
+       $./image_downloader.py -n 10 -o 100
 
+For more details, use ./image_downloader.py --help
 
-Do $python image_downloader.py -t without downloading, otherwise, images
-will be downloaded to $PWD/images automaticall.
 '''
 
 import time
 import os
 import sys
 import json
-import urllib
 import argparse
+from icrawler.examples import BingImageCrawler
 
 
 def parse_json(fname):
@@ -49,76 +53,6 @@ def check_dir(dirpath):
         os.makedirs(dirpath)
 
 
-def download_page(url):
-    """Get contents from the download page"""
-
-    url = url.encode('utf8')
-    version = (3, 0)
-    cur_version = sys.version_info
-    user_agent = ''.join(['Mozilla/5.0 (X11; Linux i686) ',
-                          'AppleWebKit/537.17(KHTML, like Gecko) ',
-                          'Chrome/24.0.1312.27 Safari/537.17'])
-    if 'useragent' in data:
-        user_agent = data['useragent']
-
-    # Python 3.0 or above
-    if cur_version >= version:
-        import urllib.request
-        try:
-            headers = {}
-            headers['User-Agent'] = user_agent
-            req = urllib.request.Request(url, headers=headers)
-            resp = urllib.request.urlopen(req)
-            respData = str(resp.read())
-            return respData
-        except Exception as e:
-            print(str(e))
-
-    # Python is 2.x
-    else:
-        import urllib2
-        try:
-            headers = {}
-            headers['User-Agent'] = user_agent
-            req = urllib2.Request(url, headers=headers)
-            response = urllib2.urlopen(req)
-            page = response.read()
-            return page
-        except:
-            return"Page Not found"
-
-
-def _images_get_next_item(s):
-    """Finding 'Next Image' from the given raw page"""
-
-    start_line = s.find('rg_di')
-    # If no links are found then give an error!
-    if start_line == -1:
-        end_quote = 0
-        link = "no_links"
-        return link, end_quote
-    else:
-        start_line = s.find('"class="rg_di"')
-        start_content = s.find('imgurl=', start_line+1)
-        end_content = s.find('&amp;', start_content+1)
-        content_raw = str(s[start_content+7:end_content])
-        return content_raw, end_content
-
-
-def _images_get_all_items(page):
-    """ Getting all links with the help of '_images_get_next_image'"""
-
-    items = []
-    while True:
-        item, end_content = _images_get_next_item(page)
-        if item == "no_links":
-            break
-        else:
-            items.append(item)
-            page = page[end_content:]
-    return items
-
-
 def main():
     parser = argparse.ArgumentParser(
         description="Simple tool to download images via Google Search."
@@ -127,61 +61,58 @@ def main():
         "-t", "--test", action='store_true'
         )
     parser.add_argument(
-        "-a", "--all", action='store_true',
-        help="Seatch for all images instead of those in 24 hours."
+        "-p", "--path", type=str, default=os.getcwd(),
+        help="Path to output images"
         )
+    parser.add_argument(
+        "-n", "--num", type=int, default=100,
+        help="How many images do you need."
+        )
+    parser.add_argument(
+        "--threads", type=int, default=4,
+        help="Number of threads to run."
+        )
+    parser.add_argument(
+        "-o", "--offset", type=int, default=0,
+        help="Offset."
+        )
+    parser.add_argument(
+        "--min", type=int, default=None,
+        help="Minimum size of the image."
+        )
+    parser.add_argument(
+        "--max", type=int, default=None,
+        help="Maximum size of the image."
+        )
+
     args = parser.parse_args()
 
     t0 = time.time()
-    results = {}
-    testfile = urllib.URLopener()
-
-    url_base = ''.join(['https://www.google.com/search?q=',
-                        'REPLACEME',
-                        '&espv=2&biw=1366&bih=667&',
-                        'site=webhp&source=lnms&tbm=isch&sa=X',
-                        '&ei=XosDVaCXD8TasATItgE&ved=0CAcQ_AUoAg'])
-    if not args.all:
-        url_base = url_base + '&tbs=itp:photo,qdr:w'
-    for search_keyword in search_for:
-        results[search_keyword] = []
-        print (" Item name = ", search_keyword)
-        search = search_keyword.replace(' ', '%20')
-        if len(search_for[search_keyword]) < 1:
-            url = url_base.replace('REPLACEME', search)
-            raw_html = (download_page(url))
-            results[search_keyword] = _images_get_all_items(raw_html)
-            continue
-        for j in range(0, len(search_for[search_keyword])):
-            pure_keyword = '%20' + search_for[search_keyword][j]
-            pure_keyword = pure_keyword.replace(' ', '%20')
-            url = url_base.replace('REPLACEME', search + pure_keyword)
-            raw_html = (download_page(url))
-            results[search_keyword] += _images_get_all_items(raw_html)
-
-    if not args.test:
-        for search_keyword in results:
-            path = './images/' + search_keyword
-            check_dir(path)
-            os.chdir(path)
-            items = results[search_keyword]
-            for i in range(0, len(items)):
-                url = items[i].encode('utf8')
-                try:
-                    print('  Downloading: ' + str(i+1) + '/' + str(len(items)))
-                    filename = os.path.basename(url)
-                    testfile.retrieve(url, filename)
-                except IOError:
-                    print(' Skipping ' + url)
-                    pass
-                except IndexError:
-                    print(' Skipping ' + url)
-                    pass
-            os.chdir('../../')
-    else:
-        import json
-        with open('./outputs.txt', 'w')as fp:
-            json.dump(results, fp)
+    check_dir(args.path)
+    for kwd in search_for:
+        subdir = os.path.join(args.path, kwd)
+        check_dir(subdir)
+        print (" Item name = ", kwd)
+        if len(search_for[kwd]) == 0:
+            bing_crawler = BingImageCrawler(subdir)
+            bing_crawler.crawl(
+                keyword=kwd, offset=args.offset, max_num=args.num,
+                feeder_thr_num=1, parser_thr_num=1,
+                downloader_thr_num=args.threads,
+                min_size=args.min, max_size=args.max)
+        else:
+            for j in range(0, len(search_for[kwd])):
+                print("    : %s" % search_for[kwd][j])
+                ssubdir = os.path.join(subdir, search_for[kwd][j])
+                check_dir(ssubdir)
+                pure_keyword = '%20' + search_for[kwd][j]
+                pure_keyword = kwd + pure_keyword.replace(' ', '%20')
+                bing_crawler = BingImageCrawler(ssubdir)
+                bing_crawler.crawl(
+                    keyword=pure_keyword, offset=args.offset,
+                    max_num=args.num, feeder_thr_num=1,
+                    parser_thr_num=1, downloader_thr_num=args.threads,
+                    min_size=args.min, max_size=args.max)
 
 if __name__ == '__main__':
     main()
