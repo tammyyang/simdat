8344d2a51229bb7fe109ba9b7f9602cef215fe25
Tammy Yang <tammy@dt42.io>
2016-04-19 14:56:29 +0800

    can support both local folder or simple http server

--- tools/html_creator.py
+++ tools/html_creator.py
@@ -1,30 +1,36 @@
 # This is an example to create a html for displaying images
-# $python html_creator.py PARENT TEMPLATE
+# $python html_creator.py IP PARENT TEMPLATE
 # PARENT: parent folder of the images (default $PWD)
 # TEMPLATE: jinja template file
 #           (default: simdat/examples/html_plots.template)
 import os
 import sys
-from simdat.core.so import image
+from simdat.core import image
 
 imtl = image.IMAGE()
 
 args = sys.argv[1:]
 if len(args) > 0:
-    parent = args[0]
+    ip = ''.join(["http://", args[0], ":8088/"])
+else:
+    ip = os.getcwd()
+
+if len(args) > 1:
+    parent = args[1]
 else:
     parent = os.getcwd()
 print("Looking for images in %s" % parent)
 
 temp = '/home/tammy/SOURCES/simdat/tools/html_plots.template'
-if len(args) > 1:
-    temp = args[1]
+if len(args) > 2:
+    temp = args[2]
 
 outf = parent + '/images.html'
 title = 'Images'
 imgs = imtl.find_images(dir_path=parent)
 imgs.sort()
 args = {'TITLE': 'Images',
+        'ip': ip,
         'imgs': imgs}
 content = imtl.read_template(temp, args)
 with open(outf, 'w') as f:
