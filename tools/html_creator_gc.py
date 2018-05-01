6f267d2419b6dd01e42b877da026b5062024d7d2
Tammy Yang <tammy@dt42.io>
2018-05-01 21:52:16 +0800

    add html_creator_gc

---
+++ tools/html_creator_gc.py
@@ -0,0 +1,40 @@
+# This is an example to create a html for displaying images
+# $python html_creator.py IP PARENT TEMPLATE
+# PARENT: parent folder of the images (default $PWD)
+# TEMPLATE: jinja template file
+#           (default: simdat/examples/html_plots.template)
+import os
+import sys
+from simdat.core import image
+
+imtl = image.IMAGE()
+
+args = sys.argv[1:]
+if len(args) > 0:
+    ip = ''.join(["http://", args[0], "/~"])
+else:
+    ip = os.getcwd()
+print(ip)
+
+if len(args) > 1:
+    parent = args[1]
+else:
+    parent = os.getcwd()
+print("Looking for images in %s" % parent)
+
+temp = '/home/tammy/SOURCES/simdat/tools/html_plots.template'
+if len(args) > 2:
+    temp = args[2]
+
+outf = parent + '/images.html'
+title = 'Images'
+imgs = imtl.find_images(dir_path=parent)
+imgs.sort()
+ip = ip + parent.replace('/home/', '').replace('public_html', '')
+args = {'TITLE': 'Images',
+        'ip': ip,
+        'imgs': imgs}
+content = imtl.read_template(temp, args)
+with open(outf, 'w') as f:
+    print('HTML contents are written to %s' % outf)
+    f.write(content)
